060bf526d440ffc63fb86d6fe3218faff5359f10
Tiago Dias <TiagoMLCDias@users.noreply.github.com>
2020-10-11 19:07:03 +0100

    Unit/Integration Tests framework setup and HelloWorldTest created.

---
+++ tests/Domain.UnitTests/HelloWorldTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Domain.UnitTests
+{
+    public class HelloWorldTest
+    {
+        private static string HelloWorldString = "HELLO WORLD";
+
+        private bool ContainsHelloWorld(string text)
+        {
+            return text.ToUpper().Contains(HelloWorldString);
+        }
+
+        [Theory]
+        [InlineData("I would say hello world!")]
+        [InlineData("Hello WoRlD")]
+        public void If_TestStringContainsHelloWorld_Then_ShouldReturnTrue(string testCase)
+        {
+            var result = ContainsHelloWorld(testCase);
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("Hello my World")]
+        public void If_TestStringContainsNotHelloWorld_Then_ShouldReturnFalse(string testCase)
+        {
+            var result = ContainsHelloWorld(testCase);
+            result.Should().BeFalse();
+        }
+    }
+}
